41760a341801aa23012f37a752a06cdd005982c1
NotRinKk <rinm4te@gmail.com>
2024-10-19 15:33:50 +0300

    Upload complete code

--- main.cpp
+++ main.cpp
@@ -14,8 +14,82 @@ path operator""_p(const char* data, std::size_t sz) {
     return path(data, data + sz);
 }
 
-// напишите эту функцию
-bool Preprocess(const path& in_file, const path& out_file, const vector<path>& include_directories);
+bool Preprocess(const path& in_file, const path& out_file, const string& name, const vector<path>& include_directories) {
+    ifstream read_file(in_file);
+    if (!read_file.is_open()) {
+        return false; 
+    }
+
+    ofstream write_file(out_file, ios::app);
+
+    static regex relative_include_reg(R"/(\s*#\s*include\s*"([^"]*)"\s*)/");
+    static regex dir_include_reg(R"/(\s*#\s*include\s*<([^>]*)>\s*)/");
+    smatch match;
+
+    size_t line_count = 0u;
+    string line;
+
+    // Только если файл найден
+    while (getline(read_file, line)) {
+        ++line_count;
+        // Если в строке содержится относительная #include-директива
+        if (regex_search(line, match, relative_include_reg)) {
+
+            path include = path((match[1]).str());
+            path p = in_file.parent_path() / include;
+
+            // Если файл не найден, поиск выполняется последовательно по всем элементам вектора
+            if(!Preprocess(p, out_file, in_file.filename().string(), include_directories)){
+                bool found;
+                for (const auto& dir_entry : include_directories) {
+                    found = Preprocess(dir_entry / include, out_file, in_file.string(), include_directories);
+                    if(found) {
+                        break;
+                    }
+                } 
+                if (!found) {
+                    cout << "unknown include file " << include.filename().string() << " at file " << name << " at line " << line_count << endl;
+                    return false;  
+                } 
+            }
+        } // Если в строке содержится "абсолютная" #include-директива
+        else if (regex_search(line, match, dir_include_reg)) {
+            path include = path((match[1]).str());
+            bool found;
+            // Поиск выполняется последовательно по всем элементам вектора
+            for (const auto& dir_entry : include_directories) {
+                found = Preprocess(dir_entry / include, out_file, in_file.string(), include_directories);
+                 if(found) {
+                    break;
+                }
+            } 
+            if (!found) {
+                cout << "unknown include file " << include.filename().string() << " at file " << name << " at line " << line_count << endl;  
+                return false;
+            } 
+        } // Если строка не содержит #include-директив, то записываем содержимое в выходной файл
+        else {
+            write_file << line << endl;
+        }
+    }
+    return true;
+}
+
+
+bool Preprocess(const path& in_file, const path& out_file, const vector<path>& include_directories) {
+    // Если не удалось открыть начальный файл,
+    ifstream read_file(in_file.string());
+    if (!read_file) {
+        return false;
+    }
+
+    ofstream  write_file(out_file, ios::app);
+    if(!write_file.is_open()) {
+        return false;
+    }
+
+    return Preprocess(in_file, out_file, in_file.string(), include_directories);
+}
 
 string GetFileContents(string file) {
     ifstream stream(file);
